455113cf4e4d2d9f5d5569447a676c04fc2c1be8
qinggniq <livewithblank@outlook.com>
2020-07-21 19:04:44 +0800

    add run_canal_adapter

--- tests/canal/run.sh
+++ tests/canal/run.sh
@@ -19,19 +19,28 @@ function run() {
     start_ts=$(run_cdc_cli tso query --pd=http://$UP_PD_HOST:$UP_PD_PORT)
 
     run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
-    TOPIC_NAME="example"
+    TOPIC_NAME="canal"
     case $SINK_TYPE in
-        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=1";;
+        kafka) SINK_URI="kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4";;
         mysql) ;&
         *) SINK_URI="mysql://root@127.0.0.1:3333/";;
     esac
 
-    run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
-    #if [ "$SINK_TYPE" == "kafka" ]; then
-    #  run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?partition-num=4"
-    #fi
+cat - >"$WORK_DIR/changefeed.toml" <<EOF
+case-sensitive = false
+[mounter]
+worker-num = 4
+[sink]
+protocol = "canal"
+dispatchers = [
+	{matcher = ['.*'], dispatcher = "table"},
+]
+EOF
+    run_cdc_cli changefeed create --start-ts=$start_ts --config="$WORK_DIR/changefeed.toml" --sink-uri="$SINK_URI"
     run_sql_file $CUR/data/use.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     run_sql_file $CUR/data/use.sql ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
+    run_canal_adapter $TOPIC_NAME row_format
+    sleep 100000
     run_sql_file $CUR/data/prepare.sql ${UP_TIDB_HOST} ${UP_TIDB_PORT}
     # sync_diff can't check non-exist table, so we check expected tables are created in downstream first
     check_table_exists row_format.multi_data_type ${DOWN_TIDB_HOST} ${DOWN_TIDB_PORT}
